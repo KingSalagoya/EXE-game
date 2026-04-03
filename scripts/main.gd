177f20228055433aa15ba0d769704a0a392ba41e
rusithhack <rusithhack@gmail.com>
2026-04-03 10:12:38 +0530

    Basic chat system

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,9 +1,12 @@
 extends Node
 
+@onready var chat_ui: Control = $GameEnviroment/Player/CameraHolder/MainCamera/ChatUI
+
 func _ready() -> void:
 	%UserInterface.visible = true
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("exit"):
-		get_tree().quit()
+		if chat_ui.visible == false:
+			get_tree().quit()
