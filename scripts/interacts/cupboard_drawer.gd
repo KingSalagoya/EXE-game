076ad6e7764a93018f59f9d87f0c338a96ad15d2
rusithhack <rusithhack@gmail.com>
2026-04-04 20:16:51 +0530

    objectives

--- scripts/interacts/cupboard_drawer.gd
+++ scripts/interacts/cupboard_drawer.gd
@@ -1,5 +1,7 @@
 extends MeshInstance3D
 
+@onready var stats_displayer: Control = get_tree().current_scene.find_child("Stats", true, false)
+
 var is_open: bool = false
 
 func _ready() -> void:
@@ -13,3 +15,7 @@ func interact() -> void:
 	else:
 		tween.tween_property(self, "position:x", -0.40, 0.5)
 		is_open = true
+		if stats_displayer:
+			stats_displayer.objectives["Open Drawer"] = stats_displayer.state.done
+			await get_tree().create_timer(1).timeout
+			stats_displayer.objectives["Open Door"] = stats_displayer.state.allowed
--- scripts/interacts/door.gd
+++ scripts/interacts/door.gd
@@ -1,10 +1,17 @@
 extends MeshInstance3D
 
+@onready var stats_displayer: Control = get_tree().current_scene.find_child("Stats", true, false)
+
 func _ready() -> void:
 	self.show()
 
 func interact() -> void:
 	if self.visible == true:
 		self.hide()
+		if stats_displayer:
+			stats_displayer.objectives["Open Door"] = stats_displayer.state.done
+			await get_tree().create_timer(1).timeout
+			stats_displayer.objectives["Build the game"] = stats_displayer.state.allowed
+
 	else:
 		self.show()
