7675acb2bb3aa7568db93ce4c3bfd68b6e6994e5
rusithhack <rusithhack@gmail.com>
2026-04-06 18:12:01 +0530

    flow upto dvd pickup

--- scripts/main.gd
+++ scripts/main.gd
@@ -16,6 +16,8 @@ func _special_objectives(_name: String) -> void:
 			change_level(level_test)
 		"collect wood":
 			change_level(level_room)
+		"grab dvd":
+			GameManager.handle_dialogue.emit(preload("res://dialogue/room#1.dialogue"), "recall_past")
 
 func change_level(new_scene: PackedScene) -> void:
 	var prev_level = level_holder.get_child(0)
