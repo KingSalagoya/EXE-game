41f2c84a720e0c92eda110de6859ed1c4de5d0ad
King Salagoya <dalugodamalee@gmail.com>
2026-04-06 09:42:12 +0530

    fixed adn cleaned optimize code

--- scripts/interacts/interact_animation.gd
+++ scripts/interacts/interact_animation.gd
@@ -1,20 +1,17 @@
 extends Node
 
-enum interact_type {animation, scene_change, wood}
-
 @export var ANIM_PLAYER: AnimationPlayer
 @export var TOGGLE: bool = true
 
 @export_category("MAIN DETAILS")
 @export var ANIM_NAME: String = "open"
-@export var INTERACT_TEXT : String = "open"
-@export var TRIGGERS_OBJECTIVE: bool = false
-@export var INTERACT_TYPE: interact_type
-@export var next_scene: PackedScene
+@export var INTERACT_TEXT: String = "open"
+@export var OBJECTIVE: String = ""
 
 @export_category("ONLY FOR TOGGLE")
 @export var SECOND_ANIM_NAME: String = "close"
 @export var SECOND_INTERACT_TEXT: String = "close"
+@export var TOGGLE_OBJECTIVE: String = ""
 
 var toggled: bool = false
 var is_anim_playing: bool = false
@@ -23,28 +20,12 @@ var next_text: String
 
 func _enter_tree() -> void:
 	interact_text = INTERACT_TEXT
-	if ANIM_PLAYER:
-		ANIM_PLAYER.animation_finished.connect(_anim_finished)
+	ANIM_PLAYER.animation_finished.connect(_anim_finished)
 
 func interact() -> void:
-	if ANIM_PLAYER and not ANIM_PLAYER.is_playing() and INTERACT_TYPE == interact_type.animation:
-		if TRIGGERS_OBJECTIVE and not toggled: GameManager.next_objective.emit(INTERACT_TEXT)
-		
+	if ANIM_PLAYER and not ANIM_PLAYER.is_playing():
 		if not TOGGLE: ANIM_PLAYER.play(ANIM_NAME)
 		elif not is_anim_playing: toggle()
-	
-	elif INTERACT_TYPE == interact_type.scene_change and next_scene:
-		var room = owner
-		var parent = room.get_parent()
-		var new_room = next_scene.instantiate()
-		parent.remove_child(room)
-		room.queue_free()
-		parent.add_child(new_room)
-		GameManager.next_objective.emit(INTERACT_TEXT)
-	
-	elif INTERACT_TYPE == interact_type.wood:
-		GameManager.emit_signal("wood_collected")
-		self.queue_free()
 
 func toggle() -> void:
 	is_anim_playing = true
@@ -59,5 +40,7 @@ func toggle() -> void:
 
 func _anim_finished(anim) -> void:
 	if anim == ANIM_NAME or anim == SECOND_ANIM_NAME:
+		if anim == ANIM_NAME and OBJECTIVE != "": GameManager.request_objective_completed.emit(OBJECTIVE)
+		elif anim == SECOND_ANIM_NAME and TOGGLE_OBJECTIVE != "": GameManager.request_objective_completed.emit(TOGGLE_OBJECTIVE)
 		is_anim_playing = false
 		interact_text = next_text
