61a649951282546c61b6441668061c9e8f1a964e
KingSalagoya <dalugodamalee@gmail.com>
2026-04-09 18:35:40 +0530

    feature

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -3,21 +3,24 @@ extends Node
 @warning_ignore("unused_signal") signal start_game
 @warning_ignore("unused_signal") signal end_game
 
+#Objectives
 @warning_ignore("unused_signal") signal request_objective_completed(objective_name: String)
 @warning_ignore("unused_signal") signal objective_completed(objective_name: String)
 @warning_ignore("unused_signal") signal objective_collected(objective_name: String)
 @warning_ignore("unused_signal") signal next_objective
 @warning_ignore("unused_signal") signal wood_collected
 
+#Labels
 @warning_ignore("unused_signal") signal update_interact_label(text: String)
 @warning_ignore("unused_signal") signal update_objective_label(text: String)
 
-
+#Dialogues
 @warning_ignore("unused_signal") signal handle_dialogue(resourse, title: String)
 @warning_ignore("unused_signal") signal chat_dialogue(num: int)
 
 
 @warning_ignore("unused_signal") signal change_scene(level: String)
+@warning_ignore("unused_signal") signal unhandled_input(input: String)
 
 var can_move: bool = true
 var inventory:Dictionary [String, int] = {
@@ -25,3 +28,8 @@ var inventory:Dictionary [String, int] = {
 	"dvd": 0,
 	"walkie_talkie": 0
 }
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("Enter"): unhandled_input.emit("enter")
+	if event.is_action_pressed("jump"): unhandled_input.emit("jump")
+	if event.is_action_pressed("exit"): get_tree().quit()
