5d374155d42d2c4a2f0d8aed1874254f33657530
rusithhack <rusithhack@gmail.com>
2026-04-10 17:08:29 +0530

    fix

--- scripts/interacts/reached_area.gd
+++ scripts/interacts/reached_area.gd
@@ -7,13 +7,16 @@ extends Area3D
 @export var is_not_player: bool = true
 
 func _ready() -> void:
+	area_entered.connect(_on_area_entered)
 	if $"../CameraHolder":
 		$"../CameraHolder".queue_free()
+	if $"../hurt_box":
+		$"../hurt_box".queue_free()
+	if $"../hit_box":
+		$"../hit_box".queue_free()
 
 func _on_area_entered(area: Area3D) -> void:
-	if area.is_not_player:
-		return
-	elif area is hurt_box:
+	if area is hurt_box:
 		GameManager.request_objective_completed.emit(objective_name)
 		GameManager.can_move = false
-		print("Reached")
+		print("Player Reached")
