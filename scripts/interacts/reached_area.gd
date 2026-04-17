7e8ed7ff1b5d502e52ca2de3ae981c9d8dbdf40a
rusithhack <rusithhack@gmail.com>
2026-04-17 12:23:40 +0530

    'Reached Area' v2.0

--- scripts/interacts/reached_area.gd
+++ scripts/interacts/reached_area.gd
@@ -2,16 +2,33 @@ class_name reached_area
 
 extends Area3D
 
-@export var objective_name: String
+enum TYPE {animation, objective}
 
-@export var is_not_player: bool = true
+@export var type: TYPE = TYPE.objective
+@export var objective_name: String
+@export var animation_player: AnimationPlayer
+@export var animation_name: String
+
+
+#@export var is_not_player: bool = true
 
 func _ready() -> void:
 	area_entered.connect(_on_area_entered)
 
 func _on_area_entered(area: Area3D) -> void:
 	if area is hurt_box:
-		GameManager.request_objective_completed.emit(objective_name)
-		GameManager.can_move = false
-		print("Player Reached")
-		#GameManager.request_objective_completed.emit("stab")
+		match type:
+			TYPE.objective:
+				_update_objective()
+			TYPE.animation:
+				_play_animation()
+
+func _update_objective() -> void:
+	GameManager.request_objective_completed.emit(objective_name)
+	GameManager.can_move = false
+	print("Player Reached | Objective Updated!")
+
+func _play_animation() -> void:
+	if animation_player and animation_name:
+					animation_player.play(animation_name)
+					print("Player Reached | " + animation_name + " Played!")
