e7ad06f204ec1f6f56e791c506ede61221db6443
King Salagoya <dalugodamalee@gmail.com>
2026-04-20 00:22:28 +0530

    fixed not register issue

--- scripts/interacts/special_area.gd
+++ scripts/interacts/special_area.gd
@@ -6,6 +6,7 @@ extends Area3D
 @export var count: int = 1
 @export var enable_after_this_objective: String = "none"
 
+var enabled: bool = false
 
 #@export var is_not_player: bool = true
 
@@ -13,19 +14,23 @@ func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 	if area_id == "": area_id = name
 
-	if enable_after_this_objective != "none":
-		monitoring = false
-		monitorable = false
-		GameManager.objective_completed.connect(check_objective)
+	if enable_after_this_objective == "none": enabled = true
+	else: enabled = false
+
+func _physics_process(_delta: float) -> void:
+	if enable_after_this_objective == GameManager.current_objective:
+		enabled = true
 
 func check_objective() -> void:
 	await(get_tree().create_timer(0.3).timeout)
 	if GameManager.current_objective != enable_after_this_objective:
+		print("im hapi")
 		monitoring = true
 		monitorable = true
 
 func _on_body_entered(body: Node3D) -> void:
-	if count > 0 and body.is_in_group(target_group) and area_id != "":
+	print_debug(name)
+	if count > 0 and body.is_in_group(target_group) and area_id != "" and enabled:
 		GameManager.special_area_entered.emit(area_id)
 		count -= 1
 		print(target_group + " reached the special area of id: " + area_id)
