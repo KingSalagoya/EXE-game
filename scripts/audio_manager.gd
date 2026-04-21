f7d1ce3dff7e2d27859420072c0363f2873ae5f2
KingSalagoya <dalugodamalee@gmail.com>
2026-04-21 18:01:08 +0530

    smol stuff

--- scripts/audio_manager.gd
+++ scripts/audio_manager.gd
@@ -8,11 +8,13 @@ var active_music_stream: AudioStreamPlayer
 @export var audio_one_shot_scene: PackedScene
 @export var audio_one_shot_clips: Dictionary[String, AudioStream]
 
+
 func play_music(audio_name:String, from_position: float = 0.0, restart: bool = false) -> void:
 	if !restart and active_music_stream and active_music_stream.name == audio_name:
 		return
 
 	active_music_stream = music_clips.get_node(audio_name)
+	active_music_stream.bus = "music"
 	active_music_stream.play(from_position)
 
 func play_audio_one_shot(audio_name: String, pos: Vector3 = Vector3.ZERO, volume_db: float = 0.0, from_position: float = 0.0) -> AudioOneShot:
@@ -20,6 +22,7 @@ func play_audio_one_shot(audio_name: String, pos: Vector3 = Vector3.ZERO, volume
 	var audio_one_shot: AudioOneShot = audio_one_shot_scene.instantiate()
 	audio_one_shot.global_pos = pos
 	audio_one_shot.stream = audio_one_shot_clips.get(audio_name)
+	audio_one_shot.bus = "sfx"
 	audio_one_shot.volume_db = volume_db
 	audio_one_shot.from_position = from_position
 	
